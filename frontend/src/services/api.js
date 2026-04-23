949b6d57cdbeecbb1c13ff3b3ab0a2665851e095
keval khunt <kevalkhunt7@gmail.com>
2026-04-23 09:52:42 +0530

    fvfaaferfba

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const API_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:5000';
+console.log(API_URL)
 
 // Create axios instance
 const api = axios.create({
